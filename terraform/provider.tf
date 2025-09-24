020e719f6cafebc55ad0bf3edd4d74be1dc4cddc
Gabriel-Lamata <rm562093@fiap.com.br>
2025-09-24 16:20:52 -0300

    Estrutura modularizada, pipeline e arquivos Terraform conforme solicitado7

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,22 +1,19 @@
 terraform {
   required_version = "1.13.1"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.11.0"
     }
   }
-
   backend "s3" {
-    bucket       = "aws-vm-tfstate-kledsonbasso"
-    key          = "tfstate"
-    region       = "us-east-1" # ou variável para dinamizar
+    bucket  = "aws-s3-tfstate-lamata"
+    key     = "tfstate"
+    region  = "us-east-1"
     use_lockfile = true
   }
+}
 
 provider "aws" {
   region = "us-east-1"
-}
-
 }
